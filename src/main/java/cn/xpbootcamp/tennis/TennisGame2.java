423fe5deed35499578f69fc0b7699686de60da1a
zdazx <zhangduo0227@163.com>
2020-03-04 11:19:43 +0800

    [zhangduo] refactor: simply clean

--- src/main/java/cn/xpbootcamp/tennis/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame2.java
@@ -6,12 +6,8 @@ public class TennisGame2 implements TennisGame {
 
     public String P1res = "";
     public String P2res = "";
-    private String player1Name;
-    private String player2Name;
 
-    public TennisGame2(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+    public TennisGame2() {
     }
 
     public String getScore() {
@@ -91,22 +87,6 @@ public class TennisGame2 implements TennisGame {
         return score;
     }
 
-    public void SetP1Score(int number) {
-
-        for (int i = 0; i < number; i++) {
-            P1Score();
-        }
-
-    }
-
-    public void SetP2Score(int number) {
-
-        for (int i = 0; i < number; i++) {
-            P2Score();
-        }
-
-    }
-
     public void P1Score() {
         P1point++;
     }
@@ -116,7 +96,7 @@ public class TennisGame2 implements TennisGame {
     }
 
     public void wonPoint(String player) {
-        if (player == "player1")
+        if (player.equals("player1"))
             P1Score();
         else
             P2Score();
